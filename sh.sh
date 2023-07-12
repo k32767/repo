1a1f78f219676a02dbc5bdb55f2b64a2dad84fcf
sandwichfunny <62876472+sandwichfunny@users.noreply.github.com>
2023-07-12 13:59:57 +0800

    new iMac~update sh path

--- sh.sh
+++ sh.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 dpkg-scanpackages -m ./debs > Packages
-rm -r -f /Users/sandwich/Documents/GitHub/repo/Packages.bz2
+rm -r -f /Users/k/Documents/GitHub/repo/Packages.bz2
 bzip2 Packages
 dpkg-scanpackages -m ./debs > Packages
